c5085748cdf8f2284cf369299b59342cc8c65446
Dawid Domiński <dawid.dominski1125@gmail.com>
2023-08-04 19:01:35 +0200

    feat: configure project, add eslint, prettierrc and husky

---
+++ .eslintrc.js
@@ -0,0 +1,24 @@
+module.exports = {
+    env: {
+        browser: true,
+        amd: true,
+        node: true,
+    },
+    extends: [
+        'eslint:recommended',
+        'plugin:react/recommended',
+        'plugin:@typescript-eslint/recommended',
+        'prettier',
+    ],
+    overrides: [],
+    parser: '@typescript-eslint/parser',
+    parserOptions: {
+        ecmaVersion: 'latest',
+        sourceType: 'module',
+    },
+    plugins: ['react', '@typescript-eslint'],
+    rules: {
+        quotes: ['error', 'single'],
+        semi: ['error', 'never'],
+    },
+}
