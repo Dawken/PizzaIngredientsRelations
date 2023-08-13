0e8b11bd7c8a002265a76b388a31b0f10b6f66de
Dawid Domiński <dawid.dominski1125@gmail.com>
2023-08-13 17:53:06 +0200

    feat: change name of PizzaCard to ItemCard

--- src/components/ui/itemCard/itemCard.tsx
+++ src/components/ui/itemCard/itemCard.tsx
@@ -1,24 +1,17 @@
 import React from 'react'
-import styles from './pizzaCard.module.scss'
+import styles from './itemCard.module.scss'
 import notFound from '../../../assets/notFound.png'
 import { PizzaType } from '../../../types/pizza'
 import { Link } from 'react-router-dom'
 
-
-const PizzaCard = ({item}: {item: PizzaType}) => {
-
+const ItemCard = ({ item }: { item: PizzaType }) => {
     return (
         <Link to={`/pizzas/${item._id}`} className={styles.pizzaCard}>
             <div className={styles.pizzaPhotoContainer}>
-                <img
-                    className={styles.pizzaPhoto}
-                    src={notFound}
-                />
+                <img className={styles.pizzaPhoto} src={notFound} />
             </div>
-            <div className={styles.pizzaName}>
-                {item.pizzaName}
-            </div>
+            <div className={styles.pizzaName}>{item.pizzaName}</div>
         </Link>
     )
 }
-export default PizzaCard
+export default ItemCard
--- src/pages/home/home.tsx
+++ src/pages/home/home.tsx
@@ -2,20 +2,18 @@ import React from 'react'
 import styles from './home.module.scss'
 import Navbar from '../../components/ui/navbar/navbar'
 import useHome from './useHome'
-import PizzaCard from '../../components/ui/pizzaCard/pizzaCard'
+import ItemCard from '../../components/ui/itemCard/itemCard'
 import { PizzaType } from '../../types/pizza'
 
-
 const Home = () => {
-
     const { pizzas } = useHome()
 
     return (
         <div className={styles.layout}>
             <Navbar />
             <div className={styles.pizzas}>
-                {pizzas?.data.map((item:PizzaType) => {
-                    return <PizzaCard item={item} key={item._id}/>
+                {pizzas?.data.map((item: PizzaType) => {
+                    return <ItemCard item={item} key={item._id} />
                 })}
             </div>
         </div>
